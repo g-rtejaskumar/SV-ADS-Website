46ad969f285ed5d7807309d37a9fa96d9744b9d1
tejas <ganeshrajeshtejaskumar@gmail.com>
2026-02-19 21:46:24 +0530

    Initial setup

---
+++ sv-ads-website/src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import HomeSection from "./sections/HomeSection";
+import AboutSection from "./sections/AboutSection";
+import ServicesSection from "./sections/ServicesSection";
+import ContactSection from "./sections/ContactSection";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HomeSection />
+      <AboutSection />
+      <ServicesSection />
+      <ContactSection />
+    </>
+  );
+}
+
+export default App;
---
+++ sv-ads-website/src/sections/HomeSection.jsx
@@ -0,0 +1,7 @@
+export default function HomeSection() {
+    return (
+        <section id="home" style={{ background: "#f4f4f4" }}>
+            <h1>Home Section</h1>
+        </section>
+    );
+}
